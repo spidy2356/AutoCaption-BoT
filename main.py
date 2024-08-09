45e991e9c2de3efaa1bdcd635b1663029072544e
somrik <101243689+spidy2356@users.noreply.github.com>
2024-08-09 21:09:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,16 +41,17 @@ def about_callback(bot, update):
 
 @AutoCaptionBot.on_message(pyrogram.filters.channel)
 def edit_caption(bot, update: pyrogram.types.Message):
-  if os.environ.get("custom_caption"):
-      motech, _ = get_file_details(update)
-      try:
-          try: update.edit(custom_caption.format(file_name=motech.file_name))
-          except pyrogram.errors.FloodWait as FloodWait:
-              asyncio.sleep(FloodWait.value)
-              update.edit(custom_caption.format(file_name=motech.file_name, mote))
-      except pyrogram.errors.MessageNotModified: pass 
-  else:
-      return
+    if os.environ.get("custom_caption"):
+        motech, mote = get_file_details(update)  # Assuming get_file_details returns mote as well
+        try:
+            update.edit(custom_caption.format(file_name=motech.file_name, mote=mote))
+        except pyrogram.errors.FloodWait as flood_wait:
+            asyncio.sleep(flood_wait.value)
+            update.edit(custom_caption.format(file_name=motech.file_name, mote=mote))
+        except pyrogram.errors.MessageNotModified:
+            pass
+    else:
+        return
     
 def get_file_details(update: pyrogram.types.Message):
   if update.media:
